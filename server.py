5a34e540f1e1218e375c52d5ce9b01a99d4e7a5b
RenauxLouis <renauxlouis@gmail.com>
2021-01-30 12:34:22 -0500

    clean

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 import os
+from shutil import rmtree
+
 from fastapi import FastAPI
-from zipfile import ZipFile
+
 from constants import DEFAULT_FOLDER, ZIP_FILES_FOLDER
 from download_10k_utils import (clean_excel, download_from_sec,
                                 get_existing_merged_fpaths,
                                 get_fpaths_from_local_ticker,
                                 get_missing_years, get_ticker_cik,
-                                merge_excel_files_across_years,
-                                get_current_date_time_as_prefix)
-from starlette.responses import FileResponse
-from shutil import rmtree
+                                merge_excel_files_across_years)
 
 app = FastAPI()
 
@@ -48,13 +47,10 @@ async def download_10k(ticker, years):
         merged_fpaths = merge_excel_files_across_years(ticker_folder, years)
 
     # TODO: Don't send the raw xlsx file
-    raw_fpaths_to_send = get_fpaths_from_local_ticker(ticker_folder, years)
-    fpaths_to_send = raw_fpaths_to_send + merged_fpaths
+    raw_fpaths_to_upload_to_s3 = get_fpaths_from_local_ticker(
+        ticker_folder,  years)
+    fpaths_to_upload_to_s3 = raw_fpaths_to_upload_to_s3 + merged_fpaths
 
-    time_now = get_current_date_time_as_prefix()
-    zipfile_fpath = os.path.join(ZIP_FILES_FOLDER, f"{time_now}.zip")
-    with ZipFile(zipfile_fpath, "w") as zip_object:
-        for fpath in fpaths_to_send:
-            zip_object.write(fpath, arcname="/".join(fpath.split("/")[1:]))
+    s3_urls = upload_files_to_s3(fpaths_to_upload_to_s3)
 
-    return FileResponse(path=zipfile_fpath, filename="example.zip")
+    return {"s3_urls": s3_urls}
