598b719a243219fe1ac868762f2b0b766b9fa894
RenauxLouis <renauxlouis@gmail.com>
2021-03-02 00:58:37 -0500

    add financial postition as option for balance sheet naming

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ BASE_URL = "http://www.sec.gov/cgi-bin/browse-edgar"
 DEFAULT_FOLDER = "DEFAULT_FOLDER"
 
 REGEX_PER_TARGET_SHEET = {
-	"balance sheet": ["balance sheet"],
+	"balance sheet": ["balance sheet", "financial position"],
 	"income": ["income", "earning", "operation"],
 	"cash": ["cash"]
 }
